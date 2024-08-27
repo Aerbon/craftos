f8cce271dc37176016a534f34b343497e7b2dd4f
Aerbon <leo7904@gmail.com>
2024-08-27 21:41:06 +0100

    created master updater

---
+++ update.lua
@@ -0,0 +1,15 @@
+-- This prefix points to my package repository
+local masterurl = "https://raw.githubusercontent.com/Aerbon/craftos/master/"
+
+print("Reading package list.")
+local packagelist = fs.open(".aepkgs/list.txt","r")
+if packagelist == nil then
+  print("Package list not found.")
+else
+  local pkgname = fs.readLine()
+  while pkgname not nil do
+    local url = masterurl .. pkgname .. "/updater.lua"
+    shell.run("wget run " .. url)
+  end
+  print("No more packages listed.")
+  packagelist.close()
